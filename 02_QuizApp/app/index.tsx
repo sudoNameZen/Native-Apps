ba65fb7dbf0234b7322ed4fca16a0f06edca0de6
sudoNameZen <imdewangannitin56@gmail.com>
2025-07-09 00:18:44 +0530

    feat: initialize QuizApp with essential configurations and dependencies
    - Added package.json with scripts and dependencies for Expo, React Navigation, and NativeWind.
    - Created tailwind.config.js for Tailwind CSS integration with NativeWind preset.
    - Set up tsconfig.json for TypeScript support with strict mode and path aliases.

---
+++ 02_QuizApp/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css"
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center m-auto"
+    >
+      <Text className="bg-red-700 text-blue text-2xl">Yes Its working well </Text>
+    </View>
+  );
+}
